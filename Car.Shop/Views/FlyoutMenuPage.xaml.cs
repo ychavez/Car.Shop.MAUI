431ddcbd5116fec7ac52f949b5d58ed08f7a6f0d
Yael Chavez <you400@gmail.com>
2022-11-14 19:59:52 -0600

    flyout page

--- Car.Shop/Views/FlyoutMenuPage.xaml.cs
+++ Car.Shop/Views/FlyoutMenuPage.xaml.cs
@@ -9,12 +9,22 @@ public partial class FlyoutMenuPage : ContentPage
         InitializeComponent();
 
         var menuList = new List<FlyoutPageItem> {
-            new FlyoutPageItem { IconSource= "map.png", Title = "Mapa" },
-            new FlyoutPageItem { IconSource = "favourite.png", Title = "Favoritos" },
-             new FlyoutPageItem { IconSource = "clipboard.png", Title = "Lista" }
+            new FlyoutPageItem { IconSource= "map.png", Title = "Mapa", Target = typeof(MapPage) },
+            new FlyoutPageItem { IconSource = "favourite.png", Title = "Favoritos", Target = typeof(FavouritePage) },
+            new FlyoutPageItem { IconSource = "clipboard.png", Title = "Lista", Target = typeof(ListPage) }
         };
 
         MenuCollection.ItemsSource = menuList;
+
+    }
+
+    private void MenuCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var item = e.CurrentSelection.FirstOrDefault() as FlyoutPageItem;
+        if (item is not null)
+        {
+            /// cambiar la pagina
+        }
 
     }
 }
